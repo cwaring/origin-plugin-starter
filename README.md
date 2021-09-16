2f8c09b3f828b57d6968e9e0964c69d06f2227ab
Chris Waring <106938+cwaring@users.noreply.github.com>
2021-09-16 18:01:11 +0100

    setup initial plugin stub

---
+++ README.md
@@ -0,0 +1,1 @@
+# origin-plugin-starter
---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import { OriginPlugin } from '@app-research/origin-vue'
+import { name, version } from '../package.json'
+
+export const setup: OriginPlugin = ({ app, router, initialState }) => {
+    // setup plugin
+
+    // app.use(plugn)
+}
+
+const plugin = (options = {}) => ({
+  name,
+  version,
+  load: setup
+})
+
+export default plugin
