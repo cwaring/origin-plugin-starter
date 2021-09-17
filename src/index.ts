c0dbfb5c3f0d25f1f47eb6275d79ccb776ec7aab
Chris Waring <106938+cwaring@users.noreply.github.com>
2021-09-17 17:45:18 +0100

    add prettier config

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,8 @@ import { OriginPlugin } from '@app-research/origin-vue'
 import { name, version } from '../package.json'
 
 export const setup: OriginPlugin = ({ app, router, initialState }) => {
-    // setup plugin
-
-    // app.use(plugn)
+  // setup plugin
+  // app.use(plugn)
 }
 
 const plugin = (options = {}) => ({
